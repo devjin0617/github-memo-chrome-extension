41b991f9ec91d62c8de00ad964a21b0c6a6642a2
cheongjin.kim <devjin0617@gmail.com>
2016-08-04 23:37:50 +0900

    Add extension code

---
+++ extension/js/github.memo.js
@@ -0,0 +1,10 @@
+var jinOpt = {
+    form : {
+        input : '<input type="text" class="form-control" placeholder="write input to project memo" style="width:655px;">',
+        button : '<button class="btn btn-default" style="width:74px;">save</button>'
+    }
+};
+
+$('.repo-list-item').append(jinOpt.form.input + jinOpt.form.button);
+
+
